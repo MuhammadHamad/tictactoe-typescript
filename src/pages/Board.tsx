81ec6cc107dbe9a47b5c4afd6d7462905ba7612c
Muhammad Hammad <itshammadofficial@gmail.com>
2023-07-27 22:05:52 +0500

    create boxes for the users

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,18 +1,24 @@
-import React from "react";
-import Square from '../components/Square'
+import React, { useState } from "react";
+import Square from "../components/Square";
 import { Grid } from "semantic-ui-react";
-import {Button} from "semantic-ui-react";
+import { Button } from "semantic-ui-react";
 
 const Board: React.FC = () => {
+  const activePlayer = Math.round(Math.random()) === 0 ? "X" : "O";
+  console.log(activePlayer);
+  
+  const [currentPlayer, setCurrentPlayer] = useState(activePlayer);
   return (
     <div className="board-wrapper">
-      <p>Its my turn</p>
+      <p>Hey {currentPlayer}, it's my turn</p>
       <Grid columns={3} centered>
         {Array(9)
           .fill(null)
           .map((item, index) => (
             <Grid.Column className="board-column" key={index}>
-                <Square />
+              <Square 
+                // onClick={() => setSquareValue()}
+              />
             </Grid.Column>
           ))}
       </Grid>
