3cc109dfb034dd2f3d6036ace608b4332542cd37
Muhammad Hammad <itshammadofficial@gmail.com>
2023-07-29 23:17:32 +0500

    remove type issue from data

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -3,17 +3,20 @@ import Square from "../components/Square";
 import { Grid } from "semantic-ui-react";
 import { Button } from "semantic-ui-react";
 
+type Player = "X" | "O" | null;
+
 const Board: React.FC = () => {
   const activePlayer = Math.round(Math.random()) === 0 ? "X" : "O";
   const [currentPlayer, setCurrentPlayer] = useState(activePlayer);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [squares, setSquares] = useState<Player[]>(Array(9).fill(null));
+  const [winner, setWinner] = useState<Player>(null);
 
   const setSquareValue = (index: number) => {
     const data = squares.map((item, ind) => {
       if (index === ind) return currentPlayer;
       return item;
     });
-    setSquares(data);
+    setSquares(data as Player[]);
     setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
   };
 
