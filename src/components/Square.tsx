3c86bf70bfe31f639ad6ba1eedbd433361ace386
Muhammad Hammad <itshammadofficial@gmail.com>
2023-07-29 22:29:51 +0500

    set current player

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const Square: React.FC = () => {
-  return <button className="board-square">X</button>;
+interface iSquareProps {
+  onClick: () => void;
+  value: "X" | "O" | null;
+}
+const Square: React.FC<iSquareProps> = ({ onClick, value }) => {
+  if (!value) {
+    return (
+      <button className="board-square" onClick={onClick}>
+        {value}
+      </button>
+    );
+  }
+
+  return (
+    <button className="board-square" disabled>
+      {value}
+    </button>
+  );
 };
 
 export default Square;
--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -5,9 +5,18 @@ import { Button } from "semantic-ui-react";
 
 const Board: React.FC = () => {
   const activePlayer = Math.round(Math.random()) === 0 ? "X" : "O";
-  console.log(activePlayer);
-  
   const [currentPlayer, setCurrentPlayer] = useState(activePlayer);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const setSquareValue = (index: number) => {
+    const data = squares.map((item, ind) => {
+      if (index === ind) return currentPlayer;
+      return item;
+    });
+    setSquares(data);
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  };
+
   return (
     <div className="board-wrapper">
       <p>Hey {currentPlayer}, it's my turn</p>
@@ -16,9 +25,7 @@ const Board: React.FC = () => {
           .fill(null)
           .map((item, index) => (
             <Grid.Column className="board-column" key={index}>
-              <Square 
-                // onClick={() => setSquareValue()}
-              />
+              <Square onClick={() => setSquareValue(index)} value={squares[index]}/>
             </Grid.Column>
           ))}
       </Grid>
